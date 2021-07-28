1ebd00db35acddde67fb7420ed5a2996535b46e5
Malika939 <84612422+Malika939@users.noreply.github.com>
2021-07-28 13:00:56 +0600

    djn

---
+++ blog/models.py
@@ -0,0 +1,81 @@
+# from django.db import models
+# from django.db.models.deletion import CASCADE
+# from django.db.models.fields.related import ForeignKey
+# from django.urls import reverse
+#
+#
+# class News(models.Model):
+#     title = models.CharField(max_length=255)
+#     content = models.TextField()
+#     create_at = models.DateTimeField(auto_now_add=True)
+#     update_at = models.DateTimeField(auto_now=True)
+#     photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+#     is_published = models.BooleanField(default=True)
+#     category = ForeignKey('Category', on_delete=CASCADE)
+#
+#     def get_absolute_url(self):
+#         return reverse('view_news', kwargs={'pk': self.pk})
+#
+#     def __str__(self):
+#         return self.title
+#
+#     class Meta:
+#         verbose_name = 'Новость'
+#         verbose_name_plural = 'Новости'
+#         ordering = ['-create_at']
+#
+# class Category(models.Model):
+#     title = models.CharField(max_length=255)
+#
+#     def get_absolute_url(self):
+#         return reverse('view_news', kwargs={'category_id': self.pk})
+#
+#     def __str__ (self):
+#         return self.title
+#
+#     class Meta:
+#         verbose_name = 'Категория'
+#         verbose_name_plural = 'Категории'
+#         ordering = ['title']
+
+
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ForeignKey
+from django.urls import reverse
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    is_published = models.BooleanField(default=True)
+    category = ForeignKey('Category', on_delete=CASCADE)
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={"pk": self.pk})
+
+    def str(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-create_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    # def get_absolute_url(self):
+    #     return reverse('category', kwargs={"category_id": self.pk})
+
+    def str(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
